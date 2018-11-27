820300981172a72939e02a67c38217676b8fbc30
Kernael92 <kernaelapuko5@gmail.com>
2018-11-27 18:07:42 +0300

    create a get_location function that takes in the location's street name as an argument

--- app/requests.py
+++ app/requests.py
@@ -1,1 +1,38 @@
 import urllib.request,json
+from .models import Location
+
+
+# Getting the location base url
+base_url = None
+# Getting the client Id
+client_id = None
+# Getting the client secret
+client_secret = None
+# Getting temporary token
+temporary_token = None
+
+def configure_request(app):
+    global base_url,client_id,client_secret,temporary_token
+    base_url = app.config['LOCATION_BASE_URL']
+    client_id = app.config['CLIENT_ID']
+    client_secret = app.config['CLIENT_SECRET']
+    temporary_token = app.config['TEMPORARY_TOKEN']
+
+def get_location(stName):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_locations_url = base_url.format(stName)
+    with urllib.request.urlopen(get_locations_url) as url:
+        get_locations_data = url.read()
+        get_locations_response = json.loads(get_locations_data)
+
+        location_results = None
+
+        if get_locations_response['candidates']:
+            location_results_list = get_locations_response['candidates']
+            location_results = process_results(location_results_list)
+    return location_results
+
+
+
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     CLIENT_ID = os.environ.get('CLIENT_ID')
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-    ARCGIS_BASE_URL = os.environ.get('ARCGIS_BASE_URL')
+    LOCATION_BASE_URL = os.environ.get('ARCGIS_BASE_URL')
     TEMPORARY_TOKEN = os.environ.get('TEMPORARY_TOKEN')
 
 
