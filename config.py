a5dcef08c30b2ccb1b80033f1fb69e33dc6d8806
Kernael92 <kernaelapuko5@gmail.com>
2018-11-27 15:46:42 +0300

    install bootstrap and initialize it

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 import os
 
 class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    CLIENT_ID = os.environ.get('CLIENT_ID')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+    ARCGIS_BASE_URL = os.environ.get('ARCGIS_BASE_URL')
+    TEMPORARY_TOKEN = os.environ.get('TEMPORARY_TOKEN')
+
+
+
     pass
 
 class ProdConfig(Config):
