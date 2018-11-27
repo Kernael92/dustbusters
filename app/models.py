3cb19d9f2ac1ba62eb384e8b8f146c89abc76ea6
Kernael92 <kernaelapuko5@gmail.com>
2018-11-27 16:42:55 +0300

    define the location class instance

--- app/models.py
+++ app/models.py
@@ -2,3 +2,8 @@ class Location:
     '''
     Location class to define Location object
     '''
+    def __init__(self,placeName,stAddr,stName,zone):
+        self.placeName = placeName
+        self.stAddr = stAddr
+        self.stName = stName
+        self.zone = zone
