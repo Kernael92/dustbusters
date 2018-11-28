16bff9a3578c3c6e174956812b687aa6497a5cc1
Kernael92 <kernaelapuko5@gmail.com>
2018-11-28 21:10:02 +0300

    initial javascript commit

--- app/models.py
+++
@@ -1,9 +0,0 @@
-class Location:
-    '''
-    Location class to define Location object
-    '''
-    def __init__(self,placeName,stAddr,stName,zone):
-        self.placeName = placeName
-        self.stAddr = stAddr
-        self.stName = stName
-        self.zone = zone
--- start.sh
+++
@@ -1,9 +0,0 @@
-export SECRET_KEY='strongkey'
-export CLIENT_ID='sSmzchqOM5Gkg2KF'
-export CLIENT_SECRET='5e376400071149848dd732659b08961a'
-export LOCATION_BASE_URL='http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=Nairobi+{}&category=&outFields=*&forStorage=false&f=pjson'
-export TEMPORARY_TOKEN='rGWkq9h7oRogcEpSwPJ83OIN2pCo-r60_CsuZYi2ibaJWiCK0gc4Nc68INPe0y-0FvZNN6q93Q63NLjcRGxHANXuBcxz03CFQr6uIMG3usSIla-PVve9w4rO_MGarFT_fjAEJPKqVTsrvxvLVB3xNw..'
-
-
-
-python3.6 manage.py server
